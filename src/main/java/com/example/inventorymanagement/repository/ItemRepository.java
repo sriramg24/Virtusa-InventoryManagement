2d120b0cefdab06ce4007650417343f674e354e4
sriramg24 <127877023+sriramg24@users.noreply.github.com>
2025-06-28 19:18:11 +0530

    Update ItemRepository.java

--- src/main/java/com/example/inventorymanagement/repository/ItemRepository.java
+++ src/main/java/com/example/inventorymanagement/repository/ItemRepository.java
@@ -6,4 +6,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ItemRepository extends JpaRepository<InventoryItem, Integer> {
+      List<InventoryItem> findByDistributor(Distributor distributor);
+
 }
